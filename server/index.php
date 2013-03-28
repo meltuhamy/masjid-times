309225ecb9b21bb807ea8f2f458331af537878a4
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-03-28 12:03:49 +0000

    Added nearest mosque request #1

--- server/index.php
+++ server/index.php
@@ -8,6 +8,9 @@ $app = new \Slim\Slim();
 $app->db = new PDO("mysql:host={$CONFIG['db_host']};dbname={$CONFIG['db_name']};port={$CONFIG['db_port']};charset=utf8", $CONFIG['db_user'], $CONFIG['db_pass']);
 $res = $app->response();
 
+//Default json
+$res['Content-Type'] = 'application/json';
+
 
 $app->get('/', function () use ($app) {
   echo json_encode('Prayer times live');
@@ -24,6 +27,21 @@ $app->get('/table/:mosqueid', function($mosqueid) use ($app){
   }
   echo json_encode($rows);
   
+});
+
+$app->get('/mosque/', function() use ($app){
+  try{
+    $req = $app->request();
+    $lat = $req->params('lat');
+    $long = $req->params('long');
+
+    $stmt = $app->db->prepare('SELECT `id`, `lat`, `long`, SQRT(POW(69.1 * (`lat` - ?), 2) + POW(69.1 * (? - `long`) * COS(`lat` / 57.3), 2)) AS distance FROM mosque HAVING distance < 1000 ORDER BY distance');
+    $stmt->execute(array($lat, $long));
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  } catch(PDOException $ex){
+
+  }
+  echo json_encode($rows);
 });
 
 $app->get('/hello/:name', function ($name) {
