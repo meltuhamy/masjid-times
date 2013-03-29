510a91e83004bb4802357ca869ce99f5838d841e
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-03-29 11:19:15 +0000

    Front end now shows next prayer and count down #6

--- client/js/main.js
+++ client/js/main.js
@@ -1,20 +1,38 @@
 var masjidTimes = newMasjidTimes();
 
+var nextPrayerCounter = function(data){
+  var newTimeLeft = data/60000;
+  var seconds = false;
+  if(newTimeLeft == 0){
+    seconds = true;
+    newTimeLeft = data/1000;
+  }
+  newTimeLeft =  Math.floor(newTimeLeft) + (seconds ? ' seconds' : ' minutes') + ' until '+ masjidTimes.next;
+  $('.nextprayercounter').html(newTimeLeft);
+}
+
 $(document).ready(function(){
   //Get the nearest mosque info and log it.
   masjidTimes.requestNearestMosque(54,-5,null,function(data){
     mosque = data.response;
+
+    //Tell masjidTimes to use this mosque from now on.
+    masjidTimes.useMosque(mosque);
+
+    //Update any mosque name place holders.
     $('.mosque-name').html(mosque.name);
 
     //Populate today's times.
-    masjidTimes.requestTodayPrayerTimes(mosque.id, function(data){
+    masjidTimes.requestTodayPrayerTimes(function(data){
       times = data.response;
-      $('.fajr-time').html(times.fajr);
-      $('.shuruq-time').html(times.shuruq);
-      $('.duhr-time').html(times.duhr);
-      $('.asr-time').html(times.asr);
-      $('.maghrib-time').html(times.maghrib);
-      $('.isha-time').html(times.isha);
+      //Go through each prayer and update its html.
+      for(var i = 0; i<masjidTimes.prayers.length; i++){
+        $('.'+masjidTimes.prayers[i]+'-time').html(times[masjidTimes.prayers[i]]);
+      }
+
+      // Check for next prayer periodically
+      masjidTimes.nextPrayerInterval(500, nextPrayerCounter);
     });
   });
 });
+
--- client/js/masjidtimes.js
+++ client/js/masjidtimes.js
@@ -5,11 +5,116 @@
  */
 var newMasjidTimes = function(config){
   // Private properties
-  private = {};
-  private.config = config;
+  private = {
+    // Store a configuration passed in
+    config: config,
+
+    //The next prayer interval id
+    nextPrayerIntervalId: {},
+
+    // The function to call when time for prayer
+    nextPrayerCallback: {},
+
+    // The frequency to check for next prayer
+    nextPrayerFrequency: {}
+  };
+
+  //Private methods
+  
+  /**
+   * Turns a time string of the form "hours:minutes" into an object
+   * with properties hours and minutes
+   * @param  {String} timeString The time string e.g. 04:00
+   * @return {Object}            Object literal with properties hours, minutes
+   */
+  private.stringToHoursMinutes = function(timeString){
+    var split = timeString.split(":");
+    return {hours: parseInt(split[0]), minutes: parseInt(split[1])};
+  }
+
+  /**
+   * Turns a time string of the form "hours:minutes" into a date object
+   * @param  {String} timeString The string to convert
+   * @return {Date}              Todays date at that time.
+   */
+  private.stringToTodayDate = function(timeString){
+    var split, now;
+    split = private.stringToHoursMinutes(timeString);
+    now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate(), split.hours, split.minutes, 0, 0);
+  }
+
+  private.calculateNextPrayer = function(fromDate){
+    var allPrayers = public.prayers;
+    var now = fromDate === undefined ? new Date() : fromDate;
+    var nearestPrayer = '';
+    var minDiff = 86400000; // 24 hours (i.e. max diff initially)
+
+    for(var i=0; i<allPrayers.length; i++){
+      var prayer = allPrayers[i]; // e.g. "fajr" is first
+      var prayerDate = private.stringToTodayDate(public.today[prayer]);
+      var diff = prayerDate - now;
+
+      // Only consider times in the future.
+      if(diff >= 0 && diff < minDiff){
+        minDiff = diff;
+        nearestPrayer = prayer;
+      }
+    }
+    return nearestPrayer;
+  }
+
+  private.prayerPassed = function(prayer){
+    return new Date() >= private.stringToTodayDate(public.today[prayer]);
+  }
+
+  private.nextPrayerChecker = function(){
+    var allPrayers = public.prayers;
+    var now = new Date();
+
+    // Check what's the nearest (next) prayer time 
+    next = public.next;   
+    if(next === undefined || private.prayerPassed(next)){
+      public.next = private.calculateNextPrayer(now);
+    }
+
+    // Update number of seconds remaining
+    public.millisecondsToNextPrayer = private.stringToTodayDate(public.today[public.next]) - now;
+
+    // Call callback
+    private.nextPrayerCallback(public.millisecondsToNextPrayer);
+  }
+
 
   // Public properties
-  public = {};
+  public = {
+    // A list of all the prayers
+    prayers: ['fajr','shuruq','duhr','asr','maghrib','isha'],
+
+    // Acts like an ENUM
+    PRAYER: {
+      fajr: 0,
+      shuruq: 1,
+      duhr: 2,
+      asr: 3,
+      maghrib: 4,
+      isha: 5
+    },
+
+    // Holds the mosque information that we're using
+    mosque: undefined,
+
+    // The today object shows today's prayer times.
+    today: undefined,
+
+    // Next prayer
+    next: undefined,
+
+    //Time remaining until the next prayer
+    millisecondsToNextPrayer: {}
+  };
+
+  //Public methods
 
   /**
    * Asynchronously does ajax request to get array of nearest mosques
@@ -42,7 +147,11 @@ var newMasjidTimes = function(config){
     });
   }
 
-  public.requestTodayPrayerTimes = function(mosqueid, callback){
+  public.useMosque = function(mosqueData){
+    public.mosque = mosqueData;
+  }
+
+  public.requestTodayPrayerTimesByID = function(mosqueid, callback){
     var date, day, month;
     date = new Date;
     day = date.getDate();
@@ -54,9 +163,35 @@ var newMasjidTimes = function(config){
       data: {'month': month, 'day': day},
       cache: true
     }).done(function(data){
+      //Update the today object
+      public.today = data;
       callback({request: {'month': month, 'day': day}, response: data});
     });
   }
+
+  /**
+   * Uses mosque data to get the mosque's prayer times.
+   * public.useMosque() should have been envoked before calling this.
+   * @param  {Function} callback The function to call when request done.
+   */
+  public.requestTodayPrayerTimes = function(callback){
+    if(public.mosque.id != undefined) public.requestTodayPrayerTimesByID(public.mosque.id, callback);
+  }
+
+  public.nextPrayerInterval = function(freq, callback){
+    private.nextPrayerFrequency = freq;
+    private.nextPrayerCallback = callback;
+
+    if(!isNaN(private.nextPrayerIntervalId)){
+      //Interval already set. Clear it.
+      clearInterval(private.nextPrayerIntervalId);
+    }
+
+    //Set the new interval
+    private.nextPrayerIntervalId = setInterval(private.nextPrayerChecker, private.nextPrayerFrequency);
+  }
+
+
 
   //Constructor
   return public;
