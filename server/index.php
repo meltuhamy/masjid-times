e3af6d3fbfc8fc6d6df1f42f942fddc04411bbf7
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-04-07 12:34:30 +0100

    Started on debug mode. #29

--- server/index.php
+++ server/index.php
@@ -14,18 +14,64 @@ function json($data){
   }
 }
 
+/**
+ * Sets the status to zero and outputs some error json
+ * @param  Slim    $app     The app reference
+ * @param  String  $message The message we wish to output
+ * @param  integer $status  The error code (HTTP)
+ */
 function json_error($app, $message, $status = 400){
   $app->response()->status($status);
-  json(array('Error'=>$message));
+  json(array('error'=>$message));
 }
 
+/**
+ * json_error with a specific message each time
+ * @param  Slim   $app     The app reference
+ * @param  string $message The message (optional)
+ */
 function db_json_error($app, $message='There is a problem with the database.'){
   json_error($app, $message, 503);
+}
+
+
+/**
+ * Regardless of the prayer/mosque, gets the same data
+ * for prayer times. This is always such that Fajr is
+ * always the next prayer, and Fajr is in 3 minutes.
+ *
+ * If $_GET['debug_offset'] is set, sets the fajr
+ * to current time + offset seconds.
+ * 
+ * @param  integer $month  The month to get
+ * @param  integer $day    The day of the month
+ * @param  string  $prayer The prayer to get
+ * @return mixed           The array/string to output
+ */
+function getDebugPrayerTimes($month, $day, $prayer){
+  $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 180;
+
+  if(!isset($month, $day, $prayer)){
+    // TODO: Get a whole year of dummy stuff
+  } else if (isset($month) && !isset($day)) {
+    // TODO: Get a whole month of dummy stuff
+  } else if(isset($month, $day) && !isset($prayer)){
+    // TODO: Get a day of dummy stuff
+  } else if(isset($month, $day, $prayer)){
+    // TODO: Get the next prayer
+    return date("H:i", time() + $offset);
+  }
+
+}
+
+function getDebugMosque(){
+
 }
 
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 
+$app->debug = isset($_GET['debug']) && $_GET['debug'] == '1';
 $app->db = new PDO("mysql:host={$CONFIG['db_host']};dbname={$CONFIG['db_name']};port={$CONFIG['db_port']};charset=utf8", $CONFIG['db_user'], $CONFIG['db_pass']);
 $res = $app->response();
 
@@ -54,6 +100,11 @@ $app->get('/table/:prayerid', function($prayerid) use ($app){
   $month = $req->params('month');
   $day = $req->params('day');
   $prayer = $req->params('prayer');
+
+  if($app->debug){
+    json(getDebugPrayerTimes($month, $day, $prayer));
+    return;
+  }
 
   try {
     $sql = 'SELECT * FROM prayertimes WHERE id = :prayerid';
