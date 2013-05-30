e6a0ab8a6075baa5d9a02a3072f89d730e235999
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-05-30 21:37:14 +0100

    Using chrome apis

---
+++ client/chrome.js
@@ -0,0 +1,34 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 780;
+  var height = 600;
+
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+    console.debug('Storage key "%s" in namespace "%s" changed. ' +
+        ' %s => %s ',
+        key,
+        namespace,
+        storageChange.oldValue,
+        storageChange.newValue);
+  }
+});
