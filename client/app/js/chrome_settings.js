9ae0802ec93c0235c36598f6c7cace01353583e0
Mohamed Eltuhamy <tuhamy@gmail.com>
2013-09-29 11:19:35 +0100

    Removed chrome settings files

--- client/app/js/chrome_settings.js
+++
@@ -1,1 +0,0 @@
-var masjidConfig = {url: 'http://localhost:8888/masjid/', debug: false, storage: chrome.storage.local};
--- client/app/js/settings.sample.js
+++
@@ -1,1 +0,0 @@
-var masjidConfig = {url: window.location.origin+'/masjid/', debug: false};
